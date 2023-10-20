400d5ff50f832b997bc2b697cdeef1edbf036afd
SujonBanik <sujon.banik004@gmail.com>
2023-10-20 21:14:22 +0600

    navbar and header

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home_page'),
-    path('about/', views.about, name='about_page')
+    path('', views.home, name='home page'),
+    path('about/', views.about, name='about page')
 ]
