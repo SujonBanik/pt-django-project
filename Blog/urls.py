1fb2d9e6c22ad323431aede6eb230d38e3c7e556
SujonBanik <sujon.banik004@gmail.com>
2023-10-26 21:29:17 +0600

    Post table create with data show into template

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home page'),
-    path('about/', views.about, name='about page')
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about')
 ]
