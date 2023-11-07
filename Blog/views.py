d696a12cd29352c2ac659b9ec0b875e561a76195
SujonBanik <sujon.banik004@gmail.com>
2023-11-07 21:27:59 +0600

    Blog Update add delate

--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # blog_post = [
 #     {
@@ -46,8 +47,34 @@ class PostCreateView(CreateView):
     model = Post
     fields = ['title','content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 class PostDetailView(DetailView):
     model = Post
 
+class PostUpdateView(UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 def about(request):
     return render(request, 'blog/about.html', {'title':'about_page'} )
+
+class PostDeleteView(UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
